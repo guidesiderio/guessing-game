58cf0fc356f19f00c2a28bfff1aa7659eeac31e0
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-11-25 21:39:14 -0300

    Loop while

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ import random
 
 numero_randomico = random.randint(1, 100)
 nome_jogador = input('Digite o nome do jogador: ')
-palpite = int(input(f'{nome_jogador}, digite o palpite: '))
 
-if palpite == numero_randomico:
-    print('Parabéns! Você acertou o número.')
-else: 
-    print('Erro! Você errou o número.') 
+palpite = int(input(f'{nome_jogador}, digite um palpite: '))
+
+while palpite != numero_randomico:
+        print('Erro! Você errou o número.\n')
+        palpite = int(input(f'{nome_jogador}, digite outro palpite: '))
+
+print('Parabéns! Você acertou o número.\n')
