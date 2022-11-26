8be5d9c86885885605dd79a85f428fcaa28a8cb5
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-11-25 22:31:28 -0300

    Update game levels

--- app.py
+++ app.py
@@ -3,23 +3,37 @@ import random
 numero_randomico = random.randint(1, 100)
 nome_jogador = input('Digite o nome do jogador: ')
 
-print('Regras do nível de jogo:')
+print('\nRegras do nível de jogo:')
 print('1- Nível 1 (fácil): acerte em menos de 10 jogadas')
 print('2- Nível 2 (médio): acerte em menos de 5 jogadas')
-print('3- Nível 3 (difícil): acerte em menos de 3 jogadas')
-nivel_jogo = int(input('Digite o nível do jogo: '))
+print('3- Nível 3 (difícil): acerte em menos de 3 jogadas\n')
 
-palpite = int(input(f'{nome_jogador}, digite um palpite: '))
+tentativas = 0
+while True:
+    nivel_jogo = int(input('Digite o nível do jogo: '))
 
-for i in range(nivel_jogo):
+    if nivel_jogo == 1:
+        tentativas = 10
+        break
+    elif nivel_jogo == 2:
+        tentativas == 5
+        break
+    elif nivel_jogo == 3:
+        tentativas = 3
+        break
+    else:
+        print('Nível inválido!\n')
+
+for i in range(tentativas):
+    palpite = int(input(f'{nome_jogador}, qual o seu palpite?: '))
 
     if palpite == numero_randomico:
         print('Parabéns! Você acertou o número.\n')
+        break
     elif palpite > numero_randomico:
         print('Errou para cima... Tente um número menor.\n')
     else:
-        print('Errou para baixo... Tente outro número maior.\n')    
+        print('Errou para baixo... Tente outro número maior.\n')
+    
 
-    palpite = int(input(f'{nome_jogador}, digite outro palpite: '))
-
-print('Parabéns! Você acertou o número.\n')
+        
