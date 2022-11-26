3caed4855454cf9e19f9c724c7c6b5344870c32d
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-11-25 21:26:40 -0300

    Import random

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-numero_randomico = 31
+import random
+
+numero_randomico = random.randint(1, 100)
 nome_jogador = input('Digite o nome do jogador: ')
 palpite = int(input(f'{nome_jogador}, digite o palpite: '))
 
 if palpite == numero_randomico:
     print('Parabéns! Você acertou o número.')
 else: 
-    print('Erro! Você errou o número.')    
+    print('Erro! Você errou o número.') 
