d2aab3677765b33d0c5740a9f59e50dfc0c6a8d7
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-11-25 21:47:23 -0300

    Add tips

--- app.py
+++ app.py
@@ -6,7 +6,12 @@ nome_jogador = input('Digite o nome do jogador: ')
 palpite = int(input(f'{nome_jogador}, digite um palpite: '))
 
 while palpite != numero_randomico:
-        print('Erro! Você errou o número.\n')
-        palpite = int(input(f'{nome_jogador}, digite outro palpite: '))
+
+    if palpite > numero_randomico:
+        print('Errou para cima... Tente um número maior.\n')
+    else:
+        print('Errou para baixo... Tente outro número.\n')    
+
+    palpite = int(input(f'{nome_jogador}, digite outro palpite: '))
 
 print('Parabéns! Você acertou o número.\n')
